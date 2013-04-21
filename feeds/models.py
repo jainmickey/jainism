9e5e970f736bb7db5312bdf88c6763e2e5a12212
Mayank Jain <jainmickey93@gmail.com>
2013-04-21 22:42:05 +0530

    models are ready

---
+++ feeds/models.py
@@ -0,0 +1,44 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.template.defaultfilters import slugify
+from django.core.urlresolvers import reverse
+
+class Tags(models.Model):
+    name = models.CharField(max_length=50)
+
+    def get_feeds(self):
+        return self.feeds_set.all()
+
+    class Meta:
+        verbose_name = 'Tag'
+        verbose_name_plural = 'Tags'
+
+    def __unicode__(self):
+        return self.name
+
+class Feed(models.Model):
+    title = models.CharField(max_length=300)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True, editable=False)
+    published = models.BooleanField(default=True)
+    tags = models.ManyToManyField(Tags)
+    author = models.ForeignKey(User, related_name="feeds")
+    slug = models.SlugField(max_length=300, blank=True, default='')
+
+    class Meta:
+        ordering = ["date", "title"]
+
+    def get_tags(self):
+        return self.tags.all()
+
+    def get_absolute_url(self):
+        return reverse('post_list')
+
+    def __unicode__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super(Post, self).save(*args, **kwargs)
---
+++ temple/models.py
@@ -0,0 +1,63 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+from django.core.urlresolvers import reverse
+from jainism.utils import unique_slugify
+from django.template.defaultfilters import slugify
+
+STATES = (
+    'Jammu and Kashmir', 'Himachal Pradesh', 'Uttarakhand', 'Punjab', 'Haryana', 'Delhi', 'Uttar Pradesh', 'Assam', 'Meghalaya', 'Manipur',\
+    'Mizoram','Nagaland', 'Sikkim', 'Tripura', 'Arunachal Pradesh', 'West Bengal', 'Rajasthan', 'Gujarat', 'Maharashtra', 'Goa',\
+    'Madhya Pradesh', 'Bihar', 'Jharkhand', 'Chhattisgarh', 'Orissa', 'Karnataka', 'Kerala', 'Tamil Nadu', 'Andhra Pradesh')
+
+class Tags(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=255)
+
+    class Meta:
+        verbose_name = 'Tag'
+        verbose_name_plural = 'Tags'
+
+    def get_Temple(self):
+        return self.temple_set.all()
+
+    def get_absolute_url(self):
+        return reverse('temples_list', kwargs={'pk': self.id})
+
+    def __unicode__(self):
+        return self.name
+
+class Address(models.Model):
+    street = models.CharField(max_length=350, blank=True, default = '')
+    city = models.CharField(max_length=100, default='')
+    state = models.CharField(max_length=40, choices=zip(sorted(STATES), sorted(STATES)))
+    pincode = models.PositiveIntegerField(blank=True, null=True)
+    country = models.CharField(max_length=120, default='India')
+
+    def __unicode__(self):
+        return unicode('%s %s' %(self.street, self.city, self.state))
+
+class Temple(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    images = models.ImageField(max_length=100, upload_to="/media", null=True, blank=True)
+    slug = models.SlugField(max_length=255)
+    location = models.ForeignKey(Address)
+    location_url = models.URLField(blank=True)
+    tags = models.ManyToManyField(Tags)
+    estd = models.PositiveIntegerField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True, editable=False)
+    author = models.ForeignKey(User, related_name="temples")
+    description = models.TextField(null=True, blank=True)
+
+    def __unicode__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('temple_detail', kwargs={'pk': self.id})
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            slug_str = '%s' %self.title
+            unique_slugify(self, slug_str)
+        super(Temple, self).save(*args, **kwargs)
